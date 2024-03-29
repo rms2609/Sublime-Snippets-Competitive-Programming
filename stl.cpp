691ac411094364551283f90e5ecbe6fedb96a8ea
RAMENDRA SINGH <singghramendra@gmail.com>
2024-03-29 16:56:27 +0530

    Create stl.cpp

---
+++ stl.cpp
@@ -0,0 +1,7 @@
+// sort based on second element, default sorting is based on first element for vectors
+bool comp(const vector<int>&a, const vector<int>&b){
+  return a[1] < b[1]; // this should return true value for the desired condition. Eg for ascending order a[1] elem should be less than b[1]
+}
+
+// use the comp in sort function
+sort(vec.begin(), vec.end(), comp);
